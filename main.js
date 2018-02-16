eca7b0cedb70355139f9b55cceb5d33ac5733ff0
Konstantin Moiseenko <konstantin.moiseenko@ab-soft.net>
2018-02-16 17:03:46 +0200

    A simple extention

---
+++ main.js
@@ -0,0 +1,49 @@
+let checkbox = $("#recaptcha-anchor div.recaptcha-checkbox-checkmark");
+let $button = $('#free_play_form_button');
+let execute = true;
+
+let check = setInterval(() => {
+	if (window.location.href.match(/https:\/\/www.google.com\/recaptcha\/api\d\/anchor/) 
+		&& checkbox.length 
+		&& checkbox.is(':visible') 
+		&& isScrolledIntoView(checkbox.get(0))) {
+
+		if (sessionStorage.getItem('accesstime')) {
+			if (new Date().getTime() - sessionStorage.getItem('accesstime') < 7000) {
+				execute = false;
+			}
+		}
+
+		if (execute) {
+			checkbox.click();
+			sessionStorage.setItem('accesstime', new Date().getTime());
+			console('Checked!');
+		}
+	}
+	clearInterval(check);
+}, 500);
+
+
+let roll = setInterval(() => {
+	if ($button.length && $button.is(':visible')) {
+		$button.click();
+		console.log('Clicked');
+	}
+	clearInterval(roll);
+}, 5000);
+
+
+
+function isScrolledIntoView(elem) {
+	let $window = $(window);
+	let $elem = $(elem);
+	let docViewTop = $window.scrollTop();
+	let docViewBottom = docViewTop + $window.height();
+	let elemTop = $elem.offset().top;
+	let elemBottom = elemTop + $elem.height();
+
+	return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+
+
+console.log('Hello world!');
