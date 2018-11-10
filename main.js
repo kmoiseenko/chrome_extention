80dff722480b0f911df050514f18a422952e2067
Konstantin Moiseenko <write.to.k.moiseenko@gmail.com>
2018-11-10 12:50:57 +0200

    Updated bonus redem system

--- main.js
+++ main.js
@@ -1,30 +1,38 @@
 (function(){
 	var freebitcoin = {
 		init: function() {
-			this.rewardBox = $('#bonus_container_free_lott p');
 			this.userRewardPoints = this.getNumber('.user_reward_points');
-			this.btcBonusPrice = this.getNumber($('#fp_bonus_rewards .effect2 .large-6')[0]);
-			this.btcBonusBtn = $('#fp_bonus_rewards .effect2 .large-3:last-child button')[0];
+
+			this.rewardBonusBox = $('#bonus_container_free_points p');
 			this.rewardBonusPrice = this.getNumber($('#free_points_rewards .effect2 .large-6')[0]);
 			this.rewardBonusBtn = $('#free_points_rewards .effect2 .large-3:last-child button')[0];
+
+			this.btcBonusBox = $('#bonus_container_fp_bonus p');
+			this.btcBonusPrice = this.getNumber($('#fp_bonus_rewards .effect2 .large-6')[0]);
+			this.btcBonusBtn = $('#fp_bonus_rewards .effect2 .large-3:last-child button')[0];
 
 			this.checkBox = $('#play_without_captchas_button');
 			this.rollBtn = $('#free_play_form_button');
 			this.checkBoxVisible = this.checkBox.is(':visible');
 			this.rollBtnVisible = this.rollBtn.is(':visible');
 
-			this.checkForRewardBox();
+			this.checkForRewardBonusBox();
+			this.checkForBtcBonusBox();
+			this.checkForCheckBox();
 		},
-		checkForRewardBox: function() {
-			if (this.rewardBox.length === 0 && this.userRewardPoints > 0) {
+		checkForRewardBonusBox: function() {
+			if (this.rewardBonusBox.length === 0 && this.userRewardPoints > 0) {
+				if (this.userRewardPoints >= this.rewardBonusPrice) {
+					this.handleClick(this.rewardBonusBtn);
+				}
+			}
+		},
+		checkForBtcBonusBox: function() {
+			if (this.btcBonusBox.length === 0 && this.userRewardPoints > 0) {
 				if (this.userRewardPoints >= (this.btcBonusPrice + this.rewardBonusPrice)) {
 					this.handleClick(this.btcBonusBtn);
 				}
-
-				this.handleClick(this.rewardBonusBtn);
 			}
-
-			this.checkForCheckBox();
 		},
 		checkForCheckBox: function() {
 			if (this.checkBoxVisible) {
